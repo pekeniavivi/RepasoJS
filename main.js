27aacd03ff9532325d84b9997f33ee31cc063f37
Silvana <silvanaspadafore@gmail.com>
2017-04-05 19:29:50 -0300

    Primera version

---
+++ main.js
@@ -0,0 +1,9 @@
+var N1 = prompt ("Ingresa un número")
+var N2 = prompt ("Ingresa otro número")
+
+var Sumar = Number(N1) + Number(N2)
+var Restar = Number(N1) - Number(N2)
+var Multiplicar = Number(N1) * Number(N2)
+var Dividir = Number(N1) / Number(N2)
+
+alert("Hola: la suma es" + " " + Sumar + ", la resta es" + " " + Restar + ", la multiplicación es" + " " + Multiplicar + " y la división es" + " " + Dividir)
